8dfd4f5b72c98ad964d6eeda9f10be721405607f
riddhidudhat22 <ridhdhidudhat2003@gmail.com>
2025-02-06 11:34:28 +0530

    admin

--- src/route/Adminroute.js
+++ src/route/Adminroute.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Layout from '../admin/component/Layout/Layout'
 import { Route, Routes } from 'react-router-dom';
 import Category from '../admin/container/Category/Category';
-import Subcategory from '../admin/container/Subcategory/Subcategory';
+
 
 function Adminroute(props) {
     return (
